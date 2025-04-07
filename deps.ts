4cca30cc71ee45d53f9cb3198498eec9e01256aa
adriangalilea <adriangalilea@gmail.com>
2025-04-07 22:33:44 +0200

    fix: update grammy dependency to v1.35.1 and bump version to 0.1.5

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 // External dependencies
-export { type Context, type NextFunction } from "https://deno.land/x/grammy@v1.19.2/mod.ts";
+export { type Context, type NextFunction } from "https://deno.land/x/grammy@v1.35.1/mod.ts";
 export { sequentialize as baseSequentialize } from "https://deno.land/x/grammy_runner@v2.0.3/mod.ts";
